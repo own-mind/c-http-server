a55bb1d7a3db0912acf2427c1c7d81c596b11e0a
Dmytro HOlovko <dmytroh@bookmap.com>
2025-12-04 21:35:16 -0700

    Simpe TCP read

--- http.c
+++ http.c
@@ -1,3 +1,7 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -7,7 +11,8 @@ typedef struct {
     int size;
 } LineArray;
 
-LineArray getLines(FILE *file) {
+LineArray getLines(int clientSockfd) {
+    FILE *file = fdopen(clientSockfd, "r");
     char line[256];
     char **result = malloc(0);
     int n = 0;
@@ -29,15 +34,39 @@ LineArray getLines(FILE *file) {
 }
 
 int main() {
-    FILE *file = fopen("message.txt", "r");
+    int sockfd;
+    struct sockaddr_in servAddr;
 
-    LineArray arr = getLines(file);
-    for (int i = 0; i < arr.size; i++) {
-        printf("read: %s\n", arr.lines[i]);
-        free(arr.lines[i]);
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) {
+        perror("Unable to create socket");
+        return 1;
     }
 
-    free(arr.lines);
+    servAddr.sin_family = AF_INET;
+    servAddr.sin_port = htons(42069);
+    inet_pton(AF_INET, "127.0.0.1", &servAddr.sin_addr);
+
+    bind(sockfd, (struct sockaddr*) &servAddr, sizeof(servAddr));
+    if (listen(sockfd, 1) < 0) {
+        perror("Unable to listen");
+        return 1;
+    }
     
-    fclose(file);
+    while (1) {
+        struct sockaddr clientAddr;
+        socklen_t clientAddrLen;
+        int clientSockfd = accept(sockfd, (struct sockaddr *) &clientAddr, &clientAddrLen);
+
+        LineArray arr = getLines(clientSockfd);
+        for (int i = 0; i < arr.size; i++) {
+            printf("read: %s\n", arr.lines[i]);
+            free(arr.lines[i]);
+        }
+        free(arr.lines);
+
+        close(clientSockfd);
+    }
+
+    close(sockfd);
 }
